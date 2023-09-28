dbc31f89ef8d1e70dcd3c1a539135fa2e4406e2f
Torstein <torstein_skare@hotmail.com>
2023-09-28 14:36:51 +0200

    grayscale

--- server.py
+++ server.py
@@ -1,39 +1,27 @@
 import picamera
+import imutils
 import numpy as np
 import socket
-import subprocess
+import ffmpeg
 
 def read_send(sock):
     camera = picamera.PiCamera()
     print("camera started")
 
-    camera.resolution = (320, 240)
-    array = np.empty((640, 480, 3), dtype=np.uint8)
+
+    camera.resolution = (320,240)
+    array = np.zeros((320, 240), dtype=np.uint8)
     while True:
-        camera.capture(array, format='rgb')
-        frame = array.tobytes()
+        frame = camera.capture(array, format='grayscale')
+        #frame = imutils.resize(frame, width = 640)
+        frame = np.array(frame)
+        compressed_frame = ffmpeg.input(frame).output('pipe:', format='png').capture()
         
+
         try:
             print("starting send")
-            ffmpeg_process = subprocess.Popen(
-                [
-                    "ffmpeg",
-                    "-f", "rawvideo",
-                    "-s", "640x480",
-                    "-pix_fmt", "rgb24",
-                    "-i", "-",
-                    "-f", "image2pipe",
-                    "-vcodec", "png",
-                    "-"
-                ],
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE
-            )
-            compressed_frame, _ = ffmpeg_process.communicate(input=frame)
             sock.sendto(compressed_frame, ("127.0.0.1", 5006))
             print(f"frame sent. size: {len(compressed_frame)}")
-            
         except socket.error as e:
             print(e)
             break
@@ -44,7 +32,10 @@ def start_server(ip, port):
     sock.bind((ip, port))
     return sock
 
+
 if __name__ == "__main__":
     MAX_UDP_PACKET_SIZE = 65536
     sock = start_server("0.0.0.0", 5006)
     read_send(sock)
+
+    
