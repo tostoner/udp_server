c782389b3c6679770863e0bb3168f9abf649c9c0
tostoner <95102280+tostoner@users.noreply.github.com>
2023-10-04 16:55:53 +0200

    Update server.py

--- server.py
+++ server.py
@@ -48,8 +48,8 @@ def start_server(ip, port):
 
 
 if __name__ == "__main__":
-    MAX_UDP_PACKET_SIZE = 65536
-    sock = start_server("10.25.46.172", 49154)
+    MAX_UDP_PACKET_SIZE = 65508
+    sock = start_server("10.25.46.172", 13)
     capture_send(sock, ('239.9.9.11', 49155))
 
 
