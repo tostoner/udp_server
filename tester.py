908a404287e67ec3c869ac0ba20a1e45925b4975
Torstein <torstein_skare@hotmail.com>
2023-10-05 12:55:36 +0200

    it works but freeze after ~10s

--- tester.py
+++ tester.py
@@ -2,26 +2,18 @@ import cv2
 import socket
 import numpy as np
 
-
 def start_client(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(("10.22.179.34", 12346))#ip til client maskina?
+    sock.bind((ip, port))  # Uncomment this line
     print("Client started")
     return sock
 
-
-
-def receive_frames(socket, server_touple):
+def receive_frames(sock):
     while True:
-        
-        msg = "give me video"
-        socket.sendto(msg.encode("utf-8"), server_touple)
         print("Waiting for frame")
-
-        data, addr = socket.recvfrom(65507)
+        data, addr = sock.recvfrom(65565) 
         print("Frame received1")
-        frame = cv2.imdecode(np.frombuffer(
-            data, dtype=np.uint8), cv2.IMREAD_GRAYSCALE)
+        frame = cv2.imdecode(np.frombuffer(data, dtype=np.uint8), cv2.IMREAD_GRAYSCALE)
         print("Frame received")
         if frame is not None:
             cv2.imshow('Received Frame', frame)
@@ -30,13 +22,12 @@ def receive_frames(socket, server_touple):
         else:
             print("Error decoding frame")
 
-    cv2.destroyAllWindows()
-    socket.close()
-
+    cv2.destroyAllWindows() 
+    sock.close() 
 
 if __name__ == "__main__":
-    IP_ADDRESS = "10.25.46.172"#samme som server socket
-    PORT = 12395 #samme som server socket
+    IP_ADDRESS = "10.22.179.34"
+    PORT = 49155
 
     client_sock = start_client(IP_ADDRESS, PORT)
-    receive_frames(client_sock, (IP_ADDRESS, PORT))
+    receive_frames(client_sock)
