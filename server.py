b68ca94a7415faf51362b718387564d6790fa3e3
Torstein <torstein_skare@hotmail.com>
2023-10-27 12:35:31 +0200

    removed threading, async instead

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import socket
-import sys
 import threading
 import queue
 import time
@@ -13,52 +12,44 @@ sys.path.append(os.path.expanduser('/home/micro/sphero-sdk-raspberrypi-python'))
 try:
     import asyncio
     import logging.config
-    from sphero_sdk.asyncio.config import logging_config
-    from sphero_sdk.common.log_level import LogLevel
-    from sphero_sdk.common.commands import api_and_shell
-    from sphero_sdk.common.commands import system_info
-    from sphero_sdk.common.commands import power
-    from sphero_sdk.common.commands import drive
-    from sphero_sdk.common.commands import sensor
-    from sphero_sdk.common.commands import connection
-    from sphero_sdk.common.commands import io
-    from sphero_sdk import LedControlAsync
-    from sphero_sdk import DriveControlAsync
-    from sphero_sdk import InfraredControlAsync
-    from sphero_sdk import SensorControlAsync
-    from sphero_sdk import RvrFwCheckAsync
+
     from sphero_sdk.asyncio.client.toys.sphero_rvr_async import SpheroRvrAsync
+    from sphero_sdk import SpheroRvrAsync
+    from sphero_sdk import SerialAsyncDal
 
 except ImportError:
     raise ImportError('Cannot import from sphero_sdk')
 
 stopflag = threading.Event()
+loop = asyncio.get_event_loop()
 
-def init_rvr():
-    fwCheck = RvrFwCheckAsync()
-    rvr = SpheroRvrAsync(fwCheck)
+async def init_rvr(loop):
+    rvr = SpheroRvrAsync(dal=SerialAsyncDal(loop))
     print("robot object created")
 
     try:
         print("waking robot")
-        rvr.wake()
-        print(f"robot awake. Battery at {rvr.get_battery_percentage()}% voltage is {rvr.get_battery_voltage_in_volts}")
-        print("getting main application version")
-        
+        await rvr.wake()
+        print("robot awake. Getting battery percentage")
+        battery_percentage = await rvr.get_battery_percentage()
+        print(f"Battery at {battery_percentage}%")
+        voltage = await rvr.get_battery_voltage_in_volts()
+        print(f"Voltage is {voltage}")
+        print("getting main application version") 
         print("setting leds")
-        rvr.set_all_leds(
+        await rvr.set_all_leds(
             led_group=RvrLedGroups.all_lights.value,
             led_brightness_values=[color for _ in range(10) for color in [0, 255, 0]]
         )
         print("leds set")
-        rvr.reset_yaw()
+        await rvr.reset_yaw()
         print("yaw reset")
         print("RVR initialized")
     except Exception as e:
         print(f"Error initializing RVR: {e}")
     return rvr
 
-def init_camera():
+async def init_camera():
     camera = cv2.VideoCapture(0)
     print("camera started")
     camera.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
@@ -66,8 +57,8 @@ def init_camera():
     print("capture frame resized")
     return camera
 
-def capture_and_compress(camera):
-    ret,frame = camera.read()
+async def capture_and_compress(camera):
+    ret, frame = camera.read()
     if ret:
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         _, encoded_frame = cv2.imencode('.jpg', frame)
@@ -78,24 +69,19 @@ def capture_and_compress(camera):
     else:
         print("frame not found")
         return None
-    
-def recv_data(sock,queue, stopflag):
 
+async def recv_data(sock, queue, stopflag):
     while not stopflag.is_set():
         try:
-            data,addr = sock.recvfrom(4096)
+            data, addr = sock.recvfrom(4096)
             data = data.decode("utf-8")
-            print(f"data recieved {data}")
-            queue.put((data, addr))
+            print(f"data received {data}")
+            await queue.put((data, addr))
         except OSError as e:
             print(f"Socket error: {str(e)}")
             break
-        if stopflag.is_set():
-            break
 
-
-
-def handle_connection(camera, myqueue, sock, stopflag, rvr):
+async def handle_connection(camera, queue, sock, stopflag, rvr):
     startVideo = False
     heading = 0
     DRIVE_REVERSE_FLAG = 0b00000001
@@ -104,101 +90,69 @@ def handle_connection(camera, myqueue, sock, stopflag, rvr):
 
     while not stopflag.is_set():
         try:
-            data,addr = myqueue.get(timeout=1)
-        except queue.Empty:
+            data, addr = await queue.get()
+            print(f"received from client: {data}")
+            if data == "video":
+                startVideo = True
+            if data == "stop_video":
+                startVideo = False
+            if data == "forward":
+                print("forward function")
+                print(f"heading is {heading}")
+                await rvr.drive_with_heading(speed = 20, heading = heading, flags = 0)
+            if data == "backward":
+                print("backward function")
+                await rvr.drive_with_heading(speed = 20, heading = heading , flags = DRIVE_REVERSE_FLAG)
+            if data == "left":
+                print("left function")
+                print(f"heading is {heading}")
+                heading = heading - 5
+            if data == "right":
+                heading = heading + 5
+            print(f" heading is {heading}")
+        except asyncio.QueueEmpty:
             print("Queue empty")
-            data = "no input"
 
-        if data == "video":
-            startVideo = True
-        if data == "stop_video":
-            startVideo = False
-        if data == "forward":
-            print("forward function")
-            print(f"heading is {heading}")
-            rvr.drive_with_heading(speed = 20, heading = heading, flags = 0)
-        if data == "backward":
-            print("backward function")
-            rvr.drive_with_heading(speed = 20, heading = heading , flags = DRIVE_REVERSE_FLAG)
-        if data == "left":
-            print("left function")
-            print(f"heading is {heading}")
-            heading = heading - 5
-        if data == "right":
-            heading = heading + 5
-
-
-        print(f" heading is {heading}")
-
-        if startVideo == True:
-            compressed_frame = capture_and_compress(camera)
-            if compressed_frame:#Kan bytte til switch case fordi det er rasksare
-                send_frame(sock, compressed_frame, addr)
-            if not compressed_frame:
+        if startVideo:
+            compressed_frame = await capture_and_compress(camera)
+            if compressed_frame:
+                await send_frame(sock, compressed_frame, addr)
+            else:
                 print("Error capturing frame")
-        
-        else:
-            if data is not None:
-                print(f"recieved from client: {data}")
-        if stopflag.is_set():
-            break
 
-
-def send_frame(sock, frame, client):
+async def send_frame(sock, frame, client):
     try:
         sock.sendto(frame, client)
         print(f"{sys.getsizeof(frame)} bytes sent")
     except socket.error as e:
-        frame_size = sys.getsizeof(frame) 
+        frame_size = sys.getsizeof(frame)
         print("Frame size:", frame_size)
         print(e)
         return False
     return True
 
-def start_server(ip, port):
+async def start_server(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((ip, port))
     print("Server started")
     return sock
+
+async def main():
+    SOCK = await start_server("10.25.46.172", 12395)
+    print(f"server tuple is {SOCK.getsockname()}")
+    camera = await init_camera()
+    print("camera initialized")
+    rvr = await init_rvr(asyncio.get_running_loop())
+    q = asyncio.Queue()
+    reciever_task = asyncio.create_task(recv_data(SOCK, q, stopflag))
+    handler_task = asyncio.create_task(handle_connection(camera, q, SOCK, stopflag, rvr))
+    await asyncio.gather(reciever_task, handler_task)
 
 def signal_handler(sig, frame):
     print('You pressed Ctrl+C!')
     stopflag.set()
 
 if __name__ == "__main__":
-    MAX_UDP_PACKET_SIZE = 65536
-    SOCK = start_server("10.25.46.172", 12395)
-    print(f"server touple is {SOCK.getsockname()}")
-    #sock = start_server("10.25.46.172", 12395)#må ditte være samme som raspi eller kan den være random?
-    camera = init_camera()
-    print("camera initialized")
-    rvr = init_rvr()
-
-
-    q = queue.Queue()
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
-
-
-    reciever_thread = threading.Thread(target=recv_data, args=(SOCK,q, stopflag))
-    handler_thread = threading.Thread(target=handle_connection, args=(camera, q, SOCK,stopflag, rvr))
-
-    
-
-    try:
-        reciever_thread.start()
-        handler_thread.start()
-
-
-        reciever_thread.join()
-        handler_thread.join()
-    except KeyboardInterrupt:
-        print("Keyboard interrupt") #SIGNALS VIRKA IKKJE PÅ WINDOWS :( KANSKJE PÅ RASPI? :)
-    finally:
-        camera.release()
-        SOCK.close()
-        rvr.close()
-
-
-            
-    
+    asyncio.run(main())
